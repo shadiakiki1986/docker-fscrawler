5657f9ae8e603729da8f2665bedc7db7e22db2f6
Shadi Akiki <shadiakiki1986@gmail.com>
2017-01-24 18:40:04 +0200

    update release after https://github.com/dadoonet/fscrawler/pull/286/files

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ set -e
 echo "install.sh fscrawler=" $FSCRAWLER_VER
 if [ "$FSCRAWLER_VER" == "2.2" ]; then
   echo "install fscrawler 2.2"
-  wget https://oss.sonatype.org/content/repositories/snapshots/fr/pilato/elasticsearch/crawler/fscrawler/2.2-SNAPSHOT/fscrawler-2.2-20170121.085715-101.zip
+  # wget https://oss.sonatype.org/content/repositories/snapshots/fr/pilato/elasticsearch/crawler/fscrawler/2.2-SNAPSHOT/fscrawler-2.2-20170121.085715-101.zip
+  # wget https://oss.sonatype.org/content/repositories/snapshots/fr/pilato/elasticsearch/crawler/fscrawler/2.2-SNAPSHOT/fscrawler-2.2-20170124.141629-126.zip
+  wget https://oss.sonatype.org/content/repositories/snapshots/fr/pilato/elasticsearch/crawler/fscrawler/2.2-SNAPSHOT/fscrawler-2.2-20170124.163124-131.zip
 else
   echo "install fscrawler 2.1"
   wget https://repo1.maven.org/maven2/fr/pilato/elasticsearch/crawler/fscrawler/2.1/fscrawler-2.1.zip
